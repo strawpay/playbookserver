53ebc876c7f26a923e588e51dd37656fc9bf2c48
cyberzac <zac@cyberzac.se>
2015-12-07 14:06:50 +0100

    Now using vault password from environment varible. Added Ansible version to index page.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,9 @@
 package controllers
 
+import java.io.{File, FileOutputStream}
+
 import akka.ConfigurationException
+import org.joda.time.{DateTimeZone, DateTime}
 import play.api.Play.current
 import play.api._
 import play.api.libs.json.{Json, JsValue}
@@ -15,10 +18,13 @@ object Application extends Controller {
   val dir = Directory(Play.configuration.getString("ansible.playbooks").get)
   if (!dir.isDirectory) throw new ConfigurationException(s"$dir is not a directory")
   val ansible = Play.configuration.getString("ansible.command").get
-  val vaultFile = Play.configuration.getString("ansible.vault_password_file").get
+  val vaultPassword = Play.configuration.getString("ansible.vault_password").get
+  val passwordFile = createTempVaultPassFile()
+  val startedAt = DateTime.now.toDateTime(DateTimeZone.UTC)
+  val ansibleVersion = s"$ansible --version" !!
 
   def index = Action {
-    Ok(views.html.index(dir, ansible, vaultFile))
+    Ok(views.html.index(dir, ansible, ansibleVersion, startedAt))
   }
 
   def play(inventoryName: String, playbookName: String): Action[JsValue] = Action(parse.json) { request =>
@@ -36,7 +42,7 @@ object Application extends Controller {
         "-v",
         "-i", inventory.toString(),
         "-e", request.body.toString(),
-        "--vault-password-file", vaultFile,
+        "--vault-password-file", passwordFile,
         playbook.toString())
       val cmdString = cmd.reduce((s, i) => s"$s $i")
       Logger.debug(s"calling ${cmdString}")
@@ -44,16 +50,20 @@ object Application extends Controller {
       if (code == 0) {
         Logger.info(s"Playbook success\ncommand: $cmdString\n$stdout")
         val message = stdout.toString.replace("\"", "'")
-        Some(Ok(Json.parse(s"""{"status":"success", "message": "$message"}""")))
+        Some(Ok(Json.parse( s"""{"status":"success", "message": "$message"}""")))
       }
       else {
         val message = s"Playbook failed, $cmdString,  exit code $code\\n$stdout\\n$stderr"
         Logger.warn(message)
         val escaped = message.replace("\"", "'")
-        Some(ServiceUnavailable((Json.parse(s"""{"status":"failed", "message": "$escaped"}"""))))
+        Some(ServiceUnavailable((Json.parse( s"""{"status":"failed", "message": "$escaped"}"""))))
       }
     }
     result.get
+  }
+
+  def ping = Action {
+    Ok.withHeaders(CACHE_CONTROL -> "no-cache")
   }
 
   private def checkPath(file: Path, hint: String): Option[Result] = {
@@ -64,8 +74,13 @@ object Application extends Controller {
     }
   }
 
-  def ping = Action {
-    Ok.withHeaders(CACHE_CONTROL -> "no-cache")
+  private def createTempVaultPassFile(): String = {
+    val passwordFile = File.createTempFile("rocannon-", "tmp")
+    passwordFile.deleteOnExit()
+    val stream = new FileOutputStream(passwordFile)
+    stream.write(vaultPassword.getBytes)
+    stream.close()
+    passwordFile.getCanonicalPath
   }
 
 }
